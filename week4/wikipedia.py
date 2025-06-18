dc3aec22e216cf6785927cfbf4eafbba6ee8cdaa
mutsumiyamaguchi <tayomuwa2020@gmail.com>
2025-06-18 18:28:04 +0900

    fixed homework1,2(homework3 unfinished)

--- week4/wikipedia.py
+++ week4/wikipedia.py
@@ -70,64 +70,103 @@ class Wikipedia:
                 print(self.titles[dst], link_count_max)
         print()
 
+    # for homwwork1
+    # 新しいデータ構造として{id:一つ上のnodeのid}というデータを格納するための準備をする関数
+    def add_initilal_data(self):
+        self.pre_id = {}
+
+        for id,val in self.titles.items():
+            self.pre_id[id] = None
+
+    # 現在のノードのidを受け取ったら初期ノードまで逆人探索し、pathを返す関数
+    def ret_path(self,node):
+        node = node
+        path = []
+
+        while node != None:
+            path.insert(0,self.titles[node])#現在参照しているnodeのtitleをpathの先頭に追加
+            previd = self.pre_id[node]
+            node = previd
+        return path
+
 
     # Homework #1: Find the shortest path.
     # 'start': A title of the start page.
     # 'goal': A title of the goal page.
     def find_shortest_path(self, start, goal):
         # BFSを実装
+        self.add_initilal_data() #データを格納することができるよう、初期化
         queue = deque() #キューを準備 
 
         start_id = [id for id, value in self.titles.items() if value == start]#初期ノードに対応するidを探索
-        queue.append((start_id[0],[start])) #(ノードid,[初期ノードからのpath])というデータ構造でキューに追加   
-        searched_nodelst = [] # 探索済みノード用リスト
+
+        queue.append(start_id[0]) #(ノードid,1つ上のノードのid)というデータ構造でキューに追加  
+        self.pre_id[start_id[0]] = None #初期ノードの親ノードはNoneなので格納 
+
+        
+        # これだと探索にO(n)かかってしまうので,
+        # set()を用いた方がいいO(1)　順番が必要じゃない時はの方がいい
+        # 辞書の書き方もあるね
+
+        # 空間計算量的にO(V**2)になってしまうから探索済みのpathを一つだけ格納しておいてそれを最後に辿るようにする
+
+        searched_nodeset = set() # キューに格納済みノード用リスト
+        searched_nodeset.add(start_id[0])# そうでなければ探索済みリストにid追加
 
         while len(queue) > 0:# キューが空でない時
             
-            now_node,path = queue.popleft() # キューの先頭のidおよび先頭からのpathを取り出す
+            now_node= queue.popleft() # キューの先頭のidおよび親nodeのidを取り出す
 
-            if self.titles[now_node] == goal:# 現在のノードidのタイトルがゴールと一致していたらpathを返す
-                return path   
-            searched_nodelst.append(now_node)# そうでなければ探索済みリストにid追加
-            
-            now_node_linked_lst = self.links[now_node]# 現在探索したノードに接続しているノードリストを準備
-
-            
+            if self.titles[now_node] == goal:# 現在のノードidのタイトルがゴールと一致していたら
+                path = self.ret_path(now_node)# pathを生成して返してもらう
+                return path 
+              
+            now_node_linked_lst = self.links[now_node]# 現在探索したノードに接続しているノードリストを準備 
             
             for nodeid in now_node_linked_lst:# ノードリスト内のそれぞれのノードに対して、探索済みでなければキューに追加
-                if nodeid not in searched_nodelst:
+                if nodeid not in searched_nodeset:
 
-                    newpath = copy.copy(path)# pathをcopy
-                    newpath.append(self.titles[nodeid])# 対応するtitlesを追加することで初期ノードからのpathを格納
-                    queue.append((nodeid,newpath)) # (id,path)というデータをキューに追加する
+                    searched_nodeset.add(nodeid)# キューに格納済みリストにid追加
+                    queue.append(nodeid) # idをキューに追加する
+                    self.pre_id[nodeid] = now_node #pre_idを保存しておくデータに追加しておく
 
         # グラフ内に目的ノードがなければNoneを返す
         return None
 
     # for homework2
+    # デバッグする時には何回計算しているかを確認するといい
 
+    # smallで実行してできていれbアルゴリズムが間違っているわけではないはず
     # pagerank関連初期化関数
+
     def add_initial_pagerank(self):
         self.pagerank = {} #pagerank格納用
         self.pagerankbuf = {} #pagerankを更新捨時のバッファ
         self.pagerank_initial = {} #pagerankbufを初期化するとき用
         self.node_counter = 0 #node数格納
+        self.allnode_pagerank = 0 #全てのノードに均等に分配されているpagerank
+        self.allnode_pagerankbuf = 0 #allnode_pagerankを更新するために用いるバッファ
 
         for id, value in self.titles.items():
             self.pagerank[id] = 1.0 #初期pagerank1.0を追加
             self.pagerankbuf[id] = 0 # pagerankbufの初期化
             self.pagerank_initial[id] = 0 #pagerank_initialの初期化
             self.node_counter += 1 #node数インクリメント
+
+    # copy = O(N) だからinitialを初期化するときにforぶん回して0にしているのと同じことをしている
     
     # 収束していなければTrue
     # 収束していればFalseを返す
     def convergence_check(self):
         # 全てのノードについて
         for id,newpagerank in self.pagerankbuf.items():
-
-            if abs(newpagerank - self.pagerank[id])**2 > 0.01:# 収束していない時 
+            
+            # 全ノード均等のページランク + そのノード特有のページランク　で実際のページランクを算出
+            if abs((self.allnode_pagerankbuf + newpagerank) - (self.allnode_pagerank + self.pagerank[id]))**2 > 0.01:# 収束していない時 
                 self.pagerank = self.pagerankbuf.copy() # pagerankをpagerankbufに更新
                 self.pagerankbuf = self.pagerank_initial.copy() # pagerankbufを初期化
+                self.allnode_pagerank = self.allnode_pagerankbuf # 均等に分配する必要のあるページランクの更新
+                self.allnode_pagerankbuf = 0 #allnode_pagerankbufの初期化
                 return True
         
         # 収束していた場合
@@ -135,25 +174,25 @@ class Wikipedia:
         return False 
         
     # random_surferアルゴリズムを実行する関数
-    def conduct_random_surfer(self):
- 
+    def execute_random_surfer(self):
+         
         for id,pagerank in self.pagerank.items():# 全てのノードでページランクの分配を実行
+            
             
             if self.links[id]:# linked_listがある時
                 
-                linkednode_pagerank = pagerank * 0.85 /len(self.links[id])# linkednodeに85%を分配
-                allnode_pagerank = pagerank * 0.15 /self.node_counter# allnodeに15％分配
+                linkednode_pagerank = (self.allnode_pagerank + pagerank) * 0.85 /len(self.links[id])# linkednodeに85%を分配
+                self.allnode_pagerankbuf += (self.allnode_pagerank+pagerank) * 0.15 /self.node_counter# allnodeに15％分配
 
                 for linkedid in self.links[id]: #linkedlistにpagerankを追加
                     self.pagerankbuf[linkedid] += linkednode_pagerank 
             
             else: # linked_listがない時
-                allnode_pagerank = pagerank / self.node_counter # allnodeに100％分配
+                self.allnode_pagerankbuf += (self.allnode_pagerank + pagerank) / self.node_counter # allnodeに100％分配
 
             # print("data structure chek",self.pagerankbuf)
 
-            for all_id,all_rank in self.pagerankbuf.items():#全てのノードにランクを分配
-                    self.pagerankbuf[all_id] += allnode_pagerank
+             
     
     # pagerank上位10ページを返す関数
     def topten_pagerank(self):
@@ -164,7 +203,7 @@ class Wikipedia:
         title_lst = [] #title格納用
 
         for id,pagerank in topten_lst:
-            title_lst.append(self.titles[id])#titleのみリストに格納
+            title_lst.append({self.titles[id]:pagerank})#titleのみリストに格納
 
         return title_lst
 
@@ -178,11 +217,15 @@ class Wikipedia:
 
         print("finished add initial pagerank")
 
+        self.counter = 0
         while checkflg:# 収束していない間は以下を実行
-            self.conduct_random_surfer()# random_surferを実行
+            
+            self.counter += 1
+            print("counter:",self.counter)
+
+            self.execute_random_surfer()# random_surferを実行
             # print("this is pagerank :",self.pagerank)  
             checkflg = self.convergence_check()# 収束チェック
-            print(checkflg)
         
         top_pagerank = self.topten_pagerank()#pagerankトップ10を格納
         print(top_pagerank)
@@ -194,7 +237,7 @@ class Wikipedia:
 
         self.longestpath = {} #新たなデータ型を用意
 
-        for id,val in self.titles.items(): # {id:path}となるようにする 
+        for id,val in self.titles.items(): # {id:preid}となるようにする 
             self.longestpath[id] = []
             
         print("add longest path finished!!!")
@@ -203,7 +246,22 @@ class Wikipedia:
     # Search the longest path with heuristics.
     # 'start': A title of the start page.
     # 'goal': A title of the goal page.
-    def find_longest_path(self, start, goal):
+
+    def make_path(self,node,prenode):
+
+        path = []
+        goal_title = self.titls[node]
+        path.append(goal_title)
+
+        node = prenode
+
+        while node!= None:
+            title = self.titles[node]
+            path.insert(0,title)
+
+            
+
+    def find_dfs_path(self, start, goal):
 
         # DFSのキューに同じノードを複数回受け入れるver.のコードで実装
         # 2回目以降の探索の場合、pathを長い方に更新していくようにする
@@ -211,11 +269,13 @@ class Wikipedia:
         # 仮に目的のデータが見つかってもreutnrせず全探索=>計算量およびメモリ使用量がとても多い
         # 無限ループになってしまうのはどうしたらいいのかわからない
 
-        
+        # じぶんのpathにあるノードには戻らないという制約を加える
+
         self.add_longestpath_data()# 新たなデータ型を用意
 
         # DFSを実装
         stack = deque() #stack準備
+        searched_node = set() #探索済み集合の準備
 
         for id,title in self.titles.items():
             if title == start:
@@ -223,13 +283,24 @@ class Wikipedia:
             elif title == goal:
                 goal_id = id #goalのtitleに一致するもののid取得
         
-        self.longestpath[start_id] = [start] #startのidとpathを格納
+        # データ構造は{id:preid}とする
+        self.longestpath[start_id] = None #startのidとを格納
         
-        stack.append((start_id,[start])) #stackに初期ノード追加
+        stack.append(start_id) #stackに初期ノード追加
+
+        # 計算量はO(N!)　pathに入る可能性はN種類の並び方を考えると計算量はO(N!)
+        # DFSのpart2の方針で考えてみる
+
+        # 次に行くlinkednodeをシャッフルすると長いところに辿り着く可能性が増えるかもしれない
+        # 最短経路を先に知っておいて、その経路から一番遠い経路を辿るようにする（局所的に一番遠い方法）
+        # DFSからとりあえず経路を探索して、一つ選ぶ、その両端の中身を一つずつ削ってその経路の中で長いものをとってくる、更新する、これを繰り返してできるだけ長いものを作成する
+        # DFSを複数回回す時にはランダム性を入れてあげた方がながいPATHが見つかるかのうせいがあがる
 
         while len(stack) > 0:
 
             now_node = stack.pop() #右から一つの要素を取得
+            searched_node.add(now_node)
+
             # pathをアップデートするか検討
             if self.longestpath[now_node[0]]:
                 pre_path = self.longestpath[now_node[0]]
@@ -240,10 +311,8 @@ class Wikipedia:
             print(linknode_lst)
 
             for nodeid in linknode_lst:
-                node_title = self.titles[nodeid]#接続ノードのidからtitle取得
-                new_path = now_node[1].copy()# 現在の親ノードのpath取得
-                new_path.append(node_title)# pathに接続ノードを追加し現在のノードのpathを構成
-                stack.append((nodeid,new_path))#stack追加
+                stack.append(nodeid)#stack追加
+                self.longestpath[nodeid] = now_node
 
         # 探索終了後
         print(self.longestpath[goal_id])
@@ -280,17 +349,17 @@ if __name__ == "__main__":
 
     # Homework #1
 
-    # path_small = wikipedia.find_shortest_path("C", "F")
+    # path_small = wikipedia.find_shortest_path("A", "E")
     # print(path_small)
 
-    # path_med = wikipedia.find_shortest_path("ソフトウエア工学", "カーネル")
+    # path_med = wikipedia.find_shortest_path("渋谷", "パレートの法則")
     # print(path_med)
 
     # path_long = wikipedia.find_shortest_path("渋谷", "小野妹子")
     # print(path_long)
 
     # # Homework #2
-    # wikipedia.find_most_popular_pages()
+    wikipedia.find_most_popular_pages()
     # Homework #3 (optional)
-    wikipedia.find_longest_path("A", "E")
+    # wikipedia.find_longest_path("A", "E")
     # wikipedia.find_longest_path("渋谷", "池袋")
